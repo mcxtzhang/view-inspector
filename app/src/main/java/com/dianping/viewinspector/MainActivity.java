c48caee695277996f96f467069aeaa9416dee1bb
zhangxutong <zhangxutong@meituan.com>
2018-04-28 15:40:54 +0800

    init

---
+++ app/src/main/java/com/dianping/viewinspector/MainActivity.java
@@ -0,0 +1,30 @@
+package com.dianping.viewinspector;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dianping.viewinspector.suspend.DebugSuspendViewService;
+import com.dianping.viewinspector.suspend.ServiceForegroundHelper;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        sActivity = this;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ServiceForegroundHelper.startService(this, new Intent(this,
+                DebugSuspendViewService.class));
+
+    }
+
+    static Activity sActivity;
+
+    public static Activity getActivity() {
+        return sActivity;
+    }
+}
